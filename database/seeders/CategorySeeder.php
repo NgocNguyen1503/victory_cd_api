074f2e4c9803970d50a27fdaa06e96c2cc73fa41
NgocNguyen1503 <feyd153@gmail.com>
2025-11-06 23:10:25 +0700

    Ađd Seeders

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+
+class CategorySeeder extends Seeder
+{
+    private $titles = [
+        'Đĩa than',
+        'Cassette',
+        'CD nhạc việt',
+        'CD nhạc quốc tế',
+        'CD hải ngoại',
+        'Giới thiệu',
+
+        'Đĩa than quốc tế',
+        'Đĩa than Việt Nam',
+
+        'Cassette quốc tế',
+        'Cassette trong nước',
+
+        'Nhạc trẻ',
+        'Nhạc trữ tình',
+        'Nhạc vàng',
+        'Nhạc tiền chiến',
+        'Thơ ca, quan họ',
+
+        'Nhạc đồng quê',
+        'Nhạc cổ điển (Classic)',
+        'Pop',
+        'Rock',
+        'Jazz',
+        'Blues',
+
+        'CD thời kỳ 1975',
+        'CD nhạc vàng'
+    ];
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        for ($i = 0; $i < 6; $i++) {
+            DB::table('categories')->insert([
+                'title' => $this->titles[$i],
+                'parent_id' => null,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]);
+        }
+
+        for ($i = 6; $i < 8; $i++) {
+            DB::table('categories')->insert([
+                'title' => $this->titles[$i],
+                'parent_id' => 1,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]);
+        }
+
+        for ($i = 8; $i < 10; $i++) {
+            DB::table('categories')->insert([
+                'title' => $this->titles[$i],
+                'parent_id' => 2,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]);
+        }
+
+        for ($i = 10; $i < 15; $i++) {
+            DB::table('categories')->insert([
+                'title' => $this->titles[$i],
+                'parent_id' => 3,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]);
+        }
+
+        for ($i = 15; $i < 21; $i++) {
+            DB::table('categories')->insert([
+                'title' => $this->titles[$i],
+                'parent_id' => 4,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]);
+        }
+
+        for ($i = 21; $i < 23; $i++) {
+            DB::table('categories')->insert([
+                'title' => $this->titles[$i],
+                'parent_id' => 5,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]);
+        }
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Helpers\FileHelper;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    use WithoutModelEvents;
+
+    private $names = [
+        'FeyD',
+        'HieuPham',
+        'SangPTS',
+    ];
+
+    private $emails = [
+        'feyd153@gmail.com',
+        'tomnguyenhieu2004@gmail.com',
+        'phamthesang1307@gmail.com',
+    ];
+
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        for ($i = 0; $i < count($this->names); $i++) {
+            $user = new User;
+            $user->email = $this->emails[$i];
+            DB::table('users')->insert([
+                'name' => $this->names[$i],
+                'email' => $this->emails[$i],
+                'password' => Hash::make('12345678'),
+                'avatar' => env('APP_URL') . '/uploads/avatars' . '/' . FileHelper::getNameFromEmail($user) . '.jpg',
+                'email_verified_at' => Carbon::now(),
+                'role' => 0,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]);
+        }
+    }
+}
